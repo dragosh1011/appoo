0354141653d50ad3c9a41faeaaafbe9d35067b85
Scripnic Alexandru <alexandru.scripnic@ascendro.de>
2018-01-18 23:36:49 +0200

    add nodemon

--- public/js/controllers/loginController.js
+++ public/js/controllers/loginController.js
@@ -1,50 +1,54 @@
 'use strict';
+class LoginController {
+  constructor(LoginService) {
+    this.LoginService = LoginService;
+  }
 
-(function(){
-  angular.module('app')
+  logout() {
+    this.LoginService.logout();
+    alert("Successful logout");
+  }
 
-  .controller("LoginController", ['$scope', 'LoginService', function($scope, LoginService){
+  login(user) {
+    this.LoginService.login(user)
+          .then(function () {
+            alert("Successful Login");
+            Object.assign(user, {
+              username: "",
+              password: ""
+            });
+          })
+          .catch(function (err) {
+            this.loginError = "Invalid username or password";
+          });
+  }
 
-    $scope.logout = function(user){
-      LoginService.logout();
-      alert("Successful logout");
-    };
-
-    $scope.login = function(user){
-      LoginService.login(user)
-      .then(function() {
-        alert("Successful Login");
-        Object.assign(user, {
-          username: "",
-          password: ""
-        });
-      })
-      .catch(function(err){
-        $scope.loginError = "Invalid username or password";
-      });
-    };
-
-    $scope.register = function(newUser){
-      if (newUser === undefined) {
-        return $scope.registerError = "Please enter both username and password";
-      }
-      if (!newUser.username || !newUser.password){
-        return $scope.registerError = "Please try again";
-      }
-      else {
-        LoginService.register(newUser)
-        .then(function() {
+  register(newUser) {
+    if (newUser === undefined) {
+      return this.registerError = "Please enter both username and password";
+    }
+    if (!newUser.username || !newUser.password) {
+      return this.registerError = "Please try again";
+    }
+    else {
+      LoginService.register(newUser)
+        .then(function () {
           alert("Successful Registration");
           Object.assign(newUser, {
             username: "",
             password: ""
           });
         })
-        .catch(function() {
-          $scope.registerError = "Please try again";
+        .catch(function () {
+          this.registerError = "Please try again";
         });
-      };
     };
+  }
 
-  }]);
-})();
+  get isLoggedIn() {
+    return this.LoginService.isLoggedIn;
+  }
+}
+
+angular.module('app')
+    .controller("LoginController", ['LoginService', LoginController]);
